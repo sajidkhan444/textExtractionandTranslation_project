1d90d111f1fd9205f7f5b3b82e249b027faf913b
Sajid Khan <sk2120225@gmail.com>
2026-02-17 12:53:34 +0500

    Fixed 404 issue by including API router in main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,24 @@
 from fastapi import FastAPI
 import asyncio
+
 from app.core.model_registry import load_all_models
 from app.batch.batch_collector import batch_collector_worker
 from app.batch.demucs_worker import demucs_worker
 from app.batch.whisper_worker import whisper_worker
 from app.batch.qwen_worker import qwen_worker
 from app.batch.translation_worker import translation_worker
+from app.api.routes import router   # 👈 ADD THIS
 
 app = FastAPI()
+
+app.include_router(router)  # 👈 ADD THIS
 
 
 @app.on_event("startup")
 async def startup():
 
-    # Load models ONCE
     load_all_models()
 
-    # Start pipeline workers
     asyncio.create_task(batch_collector_worker())
     asyncio.create_task(demucs_worker())
     asyncio.create_task(whisper_worker())
